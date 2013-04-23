de083f3a8f4cc6e8e979136815beb55532721f41
Magnus Wrenninge <magnus@pixar.com>
2013-04-23 10:49:40 -0700

    Added RSS tracking for linux

--- test/misc_tests/lib_perf_test/src/main.cpp
+++ test/misc_tests/lib_perf_test/src/main.cpp
@@ -60,6 +60,10 @@
 
 #ifdef __APPLE__
 #include <mach/mach.h>
+#elif __linux__
+#include <unistd.h>
+#include <ios>
+#include <fstream>
 #endif
 
 //----------------------------------------------------------------------------//
@@ -94,6 +98,32 @@ static size_t currentRSS()
     return -1;
   }
   return t_info.resident_size;
+#elif __linux__
+  using std::ios_base;
+  using std::ifstream;
+  using std::string;
+
+  ifstream stat_stream("/proc/self/stat", ios_base::in);
+
+  string pid, comm, state, ppid, pgrp, session, tty_nr;
+  string tpgid, flags, minflt, cminflt, majflt, cmajflt;
+  string utime, stime, cutime, cstime, priority, nice;
+  string O, itrealvalue, starttime;
+
+  unsigned long vsize;
+  long rss;
+
+  stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr
+              >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt
+              >> utime >> stime >> cutime >> cstime >> priority >> nice
+              >> O >> itrealvalue >> starttime >> vsize >> rss; // don't care about the rest
+
+  stat_stream.close();
+
+  long page_size = sysconf(_SC_PAGE_SIZE); // in case x86-64 is configured to use 2MB pages
+  // vm_usage     = vsize / 1024.0;
+  // resident_set = rss * page_size;
+  return rss * page_size;
 #endif
 }
 

f3eac80c41fd2b9f8e08a39eccf4bee25a0e4288
Sosh Mirsepassi <smirsepassi@gmail.com>
2013-04-25 14:44:15 -0700

    reverted back the boost lib version

--- BuildSupport.py
+++ BuildSupport.py
@@ -214,7 +214,7 @@ def setupEnv(env, pathToRoot = "."):
     # Hdf5 lib
     env.Append(LIBS = ["hdf5"])
     # Boost system
-    env.Append(LIBS = ["boost_system-gcc44-mt-1.51"])
+    env.Append(LIBS = ["boost_system-mt"])
     # Boost threads
     if siteExists and hasattr(Site, "boostThreadLib"):
         env.Append(LIBS = [Site.boostThreadLib])

6e437dd9c95bc8f29e5b8b2a1c6058e60942aba5
Magnus Wrenninge <magnus.wrenninge@gmail.com>
2014-07-07 12:09:11 -0700

    Adding in Ogawa files. Does not yet compile.

---
+++ include/OgIAttribute.h
@@ -0,0 +1,97 @@
+//----------------------------------------------------------------------------//
+
+#ifndef _INCLUDED_Field3D_OgIAttribute_H_
+#define _INCLUDED_Field3D_OgIAttribute_H_
+
+//----------------------------------------------------------------------------//
+// Includes
+//----------------------------------------------------------------------------//
+
+#include "OgUtil.h"
+
+//----------------------------------------------------------------------------//
+// OgIAttribute
+//----------------------------------------------------------------------------//
+
+/*! \class OgIAttribute
+  Ogawa input attribute. Reads a single attribute from an F3D-style 
+  Ogawa file.
+
+  Attributes should not be relied on for performance input of large data sets,
+  use OgIDataset for that.
+*/
+
+//----------------------------------------------------------------------------//
+
+template <typename T>
+class OgIAttribute : public OgIBase
+{
+
+public:
+
+  // Ctor, dtor ----------------------------------------------------------------
+
+  //! The default constructor leaves m_group initialized, which implies it 
+  //! is in an invalid state.
+  OgIAttribute();
+
+  //! Initialize from an existing IGroup. The constructor will check that the
+  //! type enum matches the template parameter, otherwise the attribute is
+  //! marked invalid.
+  OgIAttribute(Alembic::Ogawa::IGroupPtr group);
+
+  // Main methods --------------------------------------------------------------
+
+  //! Returns the value of the attribute. This will be zero if the attribute
+  //! is invalid.
+  T                  value() const;
+
+};
+
+//----------------------------------------------------------------------------//
+// Template implementations
+//----------------------------------------------------------------------------//
+
+template <typename T>
+OgIAttribute<T>::OgIAttribute()
+{ 
+  // Empty
+}
+
+//----------------------------------------------------------------------------//
+
+template <typename T>
+OgIAttribute<T>::OgIAttribute(Alembic::Ogawa::IGroupPtr group)
+  : OgIBase(group)
+{
+  // Handle null pointer
+  if (!OgIBase::m_group) {
+    return;
+  }
+  // Check data type
+  OgDataType dataType = readDataType(group, 2);
+  if (dataType != OgawaTypeTraits<T>::typeEnum()) {
+    OgIBase::m_group.reset();
+    return;
+  }
+  // Update name
+  getGroupName(OgIBase::m_group, OgIBase::m_name);
+}
+
+//----------------------------------------------------------------------------//
+
+template <typename T>
+T OgIAttribute<T>::value() const
+{
+  T v;
+  if (readData(m_group, 3, v)) {
+    return v;
+  }
+  return T(0);
+}
+
+//----------------------------------------------------------------------------//
+
+#endif // include guard
+
+//----------------------------------------------------------------------------//
---
+++ include/OgIGroup.h
@@ -0,0 +1,122 @@
+//----------------------------------------------------------------------------//
+
+#ifndef _INCLUDED_Field3D_OgIGroup_H_
+#define _INCLUDED_Field3D_OgIGroup_H_
+
+//----------------------------------------------------------------------------//
+// Includes
+//----------------------------------------------------------------------------//
+
+#include "OgUtil.h"
+#include "OgIAttribute.h"
+#include "OgIDataset.h"
+
+//----------------------------------------------------------------------------//
+// OgIGroup
+//----------------------------------------------------------------------------//
+
+/*! \class OgIGroup
+  Ogawa input group. Opens a group in an F3D-style Ogawa file.
+
+  Groups can contain other groups, as well as attributes and data sets.
+*/
+
+//----------------------------------------------------------------------------//
+
+class OgIGroup : public OgIBase
+{
+
+public:
+
+  // Ctor, dtor ----------------------------------------------------------------
+
+  //! Construct from archive
+  OgIGroup(Alembic::Ogawa::IArchive &archive);
+
+  // Main methods --------------------------------------------------------------
+
+  //! Returns the type
+  OgGroupType              type() const;
+
+  //! Returns a list of F3D group names (always unique)
+  std::vector<std::string> groupNames() const;
+
+  //! Returns a list of F3D attribute names (always unique)
+  std::vector<std::string> attributeNames() const;
+
+  //! Returns a list of F3D dataset names (always unique)
+  std::vector<std::string> datasetNames() const;
+
+  //! Finds an F3D group. The returned OgIGroup will not be valid if the name
+  //! wasn't found.
+  OgIGroup                 findGroup(const std::string &name) const;
+
+  //! Finds an F3D group. The returned OgIAttribute will not be valid if the 
+  //! name wasn't found.
+  template <typename T>
+  OgIAttribute<T>          findAttribute(const std::string &name) const;
+
+  //! Finds an F3D dataset. The returned OgIGroup will not be valid if the name
+  //! wasn't found.
+  template <typename T>
+  OgIDataset<T>            findDataset(const std::string &name) const;
+
+private:
+  
+  // Private ctors -------------------------------------------------------------
+
+  //! Construct invalid group
+  OgIGroup();
+
+  //! Construct by opening group
+  OgIGroup(Alembic::Ogawa::IGroupPtr group);
+
+  // Utility methods -----------------------------------------------------------
+
+  //! Finds an ogawa-level group
+  Alembic::Ogawa::IGroupPtr findGroup(const std::string &name,
+                                      const OgGroupType groupType) const;
+
+  //! Retrieves list of ogawa-level groups of the given type
+  std::vector<std::string>  groupNames(const OgGroupType groupType) const;
+
+  //! Validates the current group
+  void                      validate();
+
+};
+
+//----------------------------------------------------------------------------//
+// Template implementations
+//----------------------------------------------------------------------------//
+
+template <typename T>
+OgIAttribute<T> OgIGroup::findAttribute(const std::string &name) const
+{
+  Alembic::Ogawa::IGroupPtr group = findGroup(name, F3DAttributeType);
+
+  if (group) {
+    return OgIAttribute<T>(group);
+  }
+
+  return OgIAttribute<T>();
+}
+
+//----------------------------------------------------------------------------//
+
+template <typename T>
+OgIDataset<T> OgIGroup::findDataset(const std::string &name) const
+{
+  Alembic::Ogawa::IGroupPtr group = findGroup(name, F3DDatasetType);
+
+  if (group) {
+    return OgIDataset<T>(group);
+  }
+
+  return OgIDataset<T>();
+}
+
+//----------------------------------------------------------------------------//
+
+#endif // include guard
+
+//----------------------------------------------------------------------------//
